37fbd2d478a3d9820909dfcc9313ec280dbb3c41
= <youness.boumalek@talan.com>
2023-10-20 17:06:46 +0200

    recreate tuto

--- android/app/src/main/kotlin/com/example/screentimelimiter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/screentimelimiter/MainActivity.kt
@@ -1,6 +1,139 @@
-package com.example.screentimelimiter
+package com.ounessy.lockmode
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import android.view.ViewGroup
+import android.app.Activity
+import android.app.ActivityManager
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
+import android.os.Build.VERSION
+import android.os.Build.VERSION_CODES
+import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+
+
 
 class MainActivity: FlutterActivity() {
+  private val CHANNEL = "com.ounessy.lockmode/kiosk_mode"
+  private val eventChannelName = "com.ounessy.lockmode/kiosk_mode_stream"
+
+        private lateinit var channel: MethodChannel
+    private lateinit var eventChannel: EventChannel
+    private var activity: Activity? = null
+    private lateinit var kioskModeHandler: KioskModeStreamHandler
+
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+  call, result ->
+  if (call.method == "getBatteryLevel") {
+    val batteryLevel = getBatteryLevel()
+
+    if (batteryLevel != -1) {
+      result.success(batteryLevel)
+    } else {
+      result.error("UNAVAILABLE", "Battery level not available.", null)
+    }
+  } else {
+    result.notImplemented()
+  }
+}
+MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+  call, result ->
+  if (call.method == "getBatteryLevel") {
+    val batteryLevel = getBatteryLevel()
+
+    if (batteryLevel != -1) {
+      result.success(batteryLevel)
+    } else {
+      result.error("UNAVAILABLE", "Battery level not available.", null)
+    }
+  } else {
+    onMethodCall(call.method)
+  }
+}
+
+  }
+
+    private fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        when (call.method) {
+            "startKioskMode" -> startKioskMode(result)
+            "stopKioskMode" -> stopKioskMode(result)
+            "isInKioskMode" -> isInKioskMode(result)
+            "isManagedKiosk" -> isManagedKiosk(result)
+            else -> result.notImplemented()
+        }
+    }
+
+
+
+  private fun getBatteryLevel(): Int {
+  val batteryLevel: Int
+  if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
+    val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+    batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
+  } else {
+    val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+    batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+  }
+
+  return batteryLevel
+}
+
+    private fun startKioskMode(result: MethodChannel.Result) {
+        activity?.let { a ->
+            // ensures that startLockTask() will not throw
+            // see https://stackoverflow.com/questions/27826431/activity-startlocktask-occasionally-throws-illegalargumentexception
+            a.findViewById<ViewGroup>(android.R.id.content).getChildAt(0).post {
+                try {
+                    a.startLockTask()
+                    result.success(true)
+                    kioskModeHandler.emit()
+                } catch (e: IllegalArgumentException) {
+                    result.success(false)
+                }
+            }
+        } ?: result.success(false)
+    }
+
+    private fun stopKioskMode(result: MethodChannel.Result) {
+        activity?.stopLockTask()
+        result.success(null)
+        kioskModeHandler.emit()
+    }
+
+
+    private fun isManagedKiosk(result: MethodChannel.Result) {
+        val service = activity?.getSystemService(Context.ACTIVITY_SERVICE) as? ActivityManager
+        if (service == null) {
+            result.success(null)
+            return
+        }
+
+        result.success(service.lockTaskModeState == ActivityManager.LOCK_TASK_MODE_LOCKED)
+    }
+
+    private fun isInKioskMode(result: MethodChannel.Result) {
+        result.success(isInKioskMode())
+    }
+
+    private fun isInKioskMode(): Boolean? {
+        val service = activity?.getSystemService(Context.ACTIVITY_SERVICE) as? ActivityManager
+            ?: return null
+
+        return when (service.lockTaskModeState) {
+            ActivityManager.LOCK_TASK_MODE_PINNED,
+            ActivityManager.LOCK_TASK_MODE_LOCKED -> true
+            else -> false
+        }
+    }
+
+
 }
